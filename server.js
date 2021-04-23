fc4b8859823d6c03e81954e8730112ea5581d433
Alok Jadhav <alokjadhav007@gmail.com>
2021-04-23 20:01:35 +0530

    change in server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/rx'));
+app.use(express.static('./dist/RxJs'));
 
 app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/rx/'}),
+    res.sendFile('index.html', {root: 'dist/RxJs/'}),
 );
 
 // Start the app by listening on the default Heroku port
